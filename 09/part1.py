cdec34d3a4446771a99a46f8e37136bcef32fc49
Piotr Rusin <piotr@rusin.work>
2023-12-09 11:46:06 +0100

    09

---
+++ 09/part1.py
@@ -0,0 +1,41 @@
+import os
+import re
+import math
+
+with open('{}/input'.format(os.path.dirname(__file__)), 'r') as f:
+    lines = f.readlines()
+
+total = 0
+
+for line in lines:
+    sequence = list(map(int, re.findall(r"(-?\d+)", line)))
+    diffs = []
+    components = []
+
+    found = True
+
+    while not all([num == 0 for num in sequence]):
+        diff = [sequence[i + 1] - num for i, num in enumerate(sequence[:-1])]
+
+        try:
+            diffs.append(diff[-1])
+            components.append(sequence[-1])
+            sequence = diff
+        except IndexError:
+            found = False
+            break
+
+    if not found:
+        continue
+
+    last_component = 0
+    diff = 0
+
+    for i in range(len(components) - 1, -1, -1):
+        diff += diffs[i]
+        last_component = components[i] + diff
+
+    total += last_component
+
+print(total)
+
